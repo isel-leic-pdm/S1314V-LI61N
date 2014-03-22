8b1f47bb233282c7001f058c5a3d751e7faf57c7
Pedro Felix <pmhsfelix@gmail.com>
2014-03-22 15:05:36 +0000

    Adds multipleApps

---
+++ demos/MultipleApps/App1/src/pt/isel/mpd/multipleapps1/MainActivity.java
@@ -0,0 +1,62 @@
+package pt.isel.mpd.multipleapps1;
+
+import android.os.Bundle;
+import android.os.Process;
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningAppProcessInfo;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	private TextView _textView;
+	
+	private void println(String msg){
+		_textView.setText(_textView.getText()+"\n"+msg);
+	}
+	
+	private void showProcessInfo(){
+		int pid = Process.myPid();
+		println("pid = "+pid);
+		println("uid = "+Process.myUid());
+		println("tid = "+Process.myTid());
+				
+		ActivityManager am = (ActivityManager)this.getSystemService(ACTIVITY_SERVICE);		
+		for(RunningAppProcessInfo pi : am.getRunningAppProcesses()){
+			if(pi.pid == pid){
+				println("Process name = "+pi.processName);				
+			}			
+		}
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		_textView = (TextView) findViewById(R.id.textView1);		
+		Button btn = (Button) findViewById(R.id.button1);
+		
+		showProcessInfo();
+		
+		btn.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent("pt.isel.pdm.intent.action2");
+				startActivity(intent);
+			}			
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
